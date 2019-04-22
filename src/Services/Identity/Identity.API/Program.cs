5ce7ebdac2744f89b3e9e697c89ec77e0fb91ac4
Ratan Sunder Parai <ratanparai@gmail.com>
2019-04-22 21:15:22 +0600

    change default namespace
    closes #8

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Identity.API
+namespace OpenCodeFoundation.eSchool.Identity.API
 {
     public class Program
     {
--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Identity.API
+namespace OpenCodeFoundation.eSchool.Identity.API
 {
     public class Startup
     {

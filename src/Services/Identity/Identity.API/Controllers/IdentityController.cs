5706b5c632add42893f2912d629fd5704c10426e
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2019-11-10 23:11:33 +0600

    RootNamespace set

--- src/Services/Identity/Identity.API/Controllers/IdentityController.cs
+++ src/Services/Identity/Identity.API/Controllers/IdentityController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Identity.API.Controllers
+namespace OpenCodeFoundation.eSchool.Identity.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]

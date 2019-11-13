7d6740ca950d9f67203a3af61411d6ef727de4e9
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2019-11-13 22:28:43 +0600

    Namespaces issue fix

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -2,7 +2,7 @@ using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Identity.API
+namespace OpenCodeFoundation.eSchool.Identity.API
 {
     public class Program
     {
--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -3,9 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace Identity.API
+namespace OpenCodeFoundation.eSchool.Identity.API
 {
     public class Startup
     {

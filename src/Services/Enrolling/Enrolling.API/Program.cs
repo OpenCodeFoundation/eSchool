8f6cb5db36c7a63c139beeb8de6ff9e6f9f22afc
Ratan Sunder Parai <ratanparai@gmail.com>
2019-12-21 00:36:57 +0600

    optimize docker compose and integrate serilog

--- src/Services/Enrolling/Enrolling.API/Program.cs
+++ src/Services/Enrolling/Enrolling.API/Program.cs
@@ -1,34 +1,41 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using OpenCodeFoundation.ESchool.Services.Enrolling.Infrastructure;
 using Serilog;
-using Serilog.Events;
 
 namespace OpenCodeFoundation.ESchool.Services.Enrolling.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static readonly string Namespace = typeof(Program).Namespace;
+        public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+
+        public static int Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Verbose()
-                .Enrich.WithProperty("ApplicationContext", "Enrolling")
-                .Enrich.FromLogContext()
-                .WriteTo.Console()
-                .WriteTo.Seq("http://seq")
-                .CreateLogger();
+            var configuration = GetConfiguration();
+
+            Log.Logger = CreateSerilogLogger(configuration);
 
             try
             {
-                Log.Information("Starting web host");
-                CreateHostBuilder(args).Build()
-                    .MigrateDbContext<EnrollingContext>((_, __) => { })
-                    .Run();
+                Log.Information("Configuring web host ({ApplicationContext})...", AppName);
+                var host = CreateHostBuilder(configuration, args).Build();
+
+                Log.Information("Applying migrations ({ApplicationContext})...", AppName);
+                host.MigrateDbContext<EnrollingContext>((_, __) => { });
+
+                Log.Information("Starting web host ({ApplicationContext})...", AppName);
+                host.Run();
+
+                return 0;
             }
             catch (Exception ex)
             {
                 Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
             }
             finally
             {
@@ -36,12 +43,37 @@ namespace OpenCodeFoundation.ESchool.Services.Enrolling.API
             }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(IConfiguration configuration, string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseConfiguration(configuration);
                     webBuilder.UseSerilog();
                 });
+
+        private static ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", AppName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://seq")
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            // Load other configurations here. Ex. Keyvault or AppConfiguration
+
+            return builder.Build();
+        }
     }
 }

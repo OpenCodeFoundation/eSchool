84a60b5c5ba3d79bd57a000ffdcaecb58947eae5
leadsoft/mahedee <mahedee.hasan@gmail.com>
2020-03-29 22:08:13 +0600

    added new domain joining

---
+++ src/Services/Joining/Joining.API/Startup.cs
@@ -0,0 +1,91 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MediatR;
+using OpenCodeFoundation.ESchool.Services.Joining.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using FluentValidation.AspNetCore;
+using OpenCodeFoundation.ESchool.Services.Joining.API.Application.Validations;
+using OpenCodeFoundation.ESchool.Services.Joining.API.Extensions;
+using Microsoft.OpenApi.Models;
+using HealthChecks.UI.Client;
+
+namespace OpenCodeFoundation.ESchool.Services.Joining.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly);
+
+            services.AddDbContext<JoiningContext>(options =>
+            {
+                options.UseSqlServer(
+                    Configuration["ConnectionStrings"],
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                    });
+            });
+
+            services.AddControllers()
+                .AddFluentValidation(fv =>
+                    fv.RegisterValidatorsFromAssemblyContaining<JoinApplicationCommandValidator>());
+
+            services.AddCustomHealthChecks(Configuration);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Enrolling HTTP API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+                });
+                endpoints.MapHealthChecks("/liveness", new HealthCheckOptions()
+                {
+                    Predicate = r => r.Name.Contains("self"),
+                });
+            });
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Enrolling HTTP API");
+                });
+        }
+    }
+}

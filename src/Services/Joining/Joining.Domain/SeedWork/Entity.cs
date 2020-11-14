d5835ca5eca40b89fd7a54c7ddb87a41189ab9a3
Ratan Sunder Parai <ratanparai@gmail.com>
2020-11-14 18:40:16 +0600

    feat(joining): remove joining service (#234)
    After initial domain analysis, we figure out that, we do not need this service.
    
    Closes #169

--- src/Services/Joining/Joining.Domain/SeedWork/Entity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace OpenCodeFoundation.ESchool.Services.Joining.Domain.SeedWork
-{
-    public abstract class Entity
-    {
-        public Guid Id { get; protected set; }
-    }
-}

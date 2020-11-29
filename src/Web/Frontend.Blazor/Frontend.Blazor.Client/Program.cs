1a2f79ce6b6d3c60934bc4b1be5e335fef793c86
Ratan Sunder Parai <ratan.parai@selise.ch>
2020-11-30 00:54:26 +0600

    add mud blazor

--- src/Web/Frontend.Blazor/Frontend.Blazor.Client/Program.cs
+++ src/Web/Frontend.Blazor/Frontend.Blazor.Client/Program.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using MudBlazor;
+using MudBlazor.Services;
 
 namespace OpenCodeFoundation.ESchool.Web.Frontend.Blazor.Client
 {
@@ -18,6 +20,10 @@ namespace OpenCodeFoundation.ESchool.Web.Frontend.Blazor.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddMudBlazorDialog();
+            builder.Services.AddMudBlazorSnackbar();
+            builder.Services.AddMudBlazorResizeListener();
 
             await builder.Build().RunAsync();
         }

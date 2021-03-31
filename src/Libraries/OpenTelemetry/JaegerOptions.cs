febe9ec81a68d53f871e4f04aaf843d19e5356a6
Ratan Sunder Parai <ratan.parai@selise.ch>
2021-04-01 02:51:52 +0600

    lib nullable

---
+++ src/Libraries/OpenTelemetry/JaegerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenCodeFoundation.OpenTelemetry
+{
+    public class JaegerOptions
+    {
+        public bool Enabled { get; set; }
+
+        public string? ServiceName { get; set; }
+
+        public string? Host { get; set; }
+
+        public int? Port { get; set; }
+    }
+}
--- src/Libraries/OpenTelemetry/OpenTelemetryOptions.cs
+++ src/Libraries/OpenTelemetry/OpenTelemetryOptions.cs
@@ -5,13 +5,13 @@ namespace OpenCodeFoundation.OpenTelemetry
     public class OpenTelemetryOptions
         : IConfigurationOptions
     {
-        public bool Enabled { get; set; } = false;
+        public bool Enabled { get; set; }
 
         public bool AlwaysOnSampler { get; set; } = true;
 
-        public bool Istio { get; set; } = false;
+        public bool Istio { get; set; }
 
-        public JaegerOptions Jaeger { get; set; } = new JaegerOptions();
+        public JaegerOptions Jaeger { get; set; } = new ();
 
         public void Validate()
         {
@@ -33,16 +33,5 @@ namespace OpenCodeFoundation.OpenTelemetry
                 throw new ArgumentException("Jaeger Host can not be null if Jaeger is enabled");
             }
         }
-    }
-
-    public class JaegerOptions
-    {
-        public bool Enabled { get; set; } = false;
-
-        public string ServiceName { get; set; }
-
-        public string Host { get; set; }
-
-        public int Port { get; set; }
     }
 }

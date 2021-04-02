691ffc2f9d693ca3d2d413f6e761173a659bca10
Ratan Sunder Parai <ratan.parai@selise.ch>
2021-04-02 20:31:44 +0600

    nullable webstatus

--- src/Web/WebStatus/Startup.cs
+++ src/Web/WebStatus/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +24,10 @@ namespace OpenCodeFoundation.ESchool.Web.WebStatus
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Performance",
+            "CA1822:Mark members as static",
+            Justification = "This method is called from framework")]
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())

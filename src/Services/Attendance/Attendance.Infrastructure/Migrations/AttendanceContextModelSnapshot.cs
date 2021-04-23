b3db5ccb464c4d13e674a113d38d641c64812411
Md. Aftab Uddin Kajal <kajal.alpha@gmail.com>
2021-04-24 05:56:00 +0600

    Update appings and fix typo error

--- src/Services/Attendance/Attendance.Infrastructure/Migrations/AttendanceContextModelSnapshot.cs
+++ src/Services/Attendance/Attendance.Infrastructure/Migrations/AttendanceContextModelSnapshot.cs
@@ -3,7 +3,6 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using OpenCodeFoundation.ESchool.Services.Attendance.Infrastructure;
 
 namespace OpenCodeFoundation.ESchool.Services.Attending.Infrastructure.Migrations
 {

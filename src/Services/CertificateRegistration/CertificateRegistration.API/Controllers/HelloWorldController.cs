c093be6b15162aa28a5e900c8c951f88383c2ccf
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-05-01 09:47:58 +0600

    added api project

---
+++ src/Services/CertificateRegistration/CertificateRegistration.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CertificateRegistration.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public static string Ping()
+        {
+            return "Pong";
+        }
+    }
+}

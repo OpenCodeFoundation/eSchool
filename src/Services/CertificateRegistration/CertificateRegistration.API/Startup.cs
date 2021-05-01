a72d03cf0e7e6e31cf87b5e85cecd5772071ec1f
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-05-01 23:34:41 +0600

    removed app.UseHttpsRedirection(); and moved swagger from development environment dependency

--- src/Services/CertificateRegistration/CertificateRegistration.API/Startup.cs
+++ src/Services/CertificateRegistration/CertificateRegistration.API/Startup.cs
@@ -41,10 +41,11 @@ namespace CertificateRegistration.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CertificateRegistration.API v1"));
             }
 
-            app.UseHttpsRedirection();
+             app.UseSwaggerUI(
+                 c => c.SwaggerEndpoint(
+                     "/swagger/v1/swagger.json", "CertificateRegistration.API v1"));
 
             app.UseRouting();
 

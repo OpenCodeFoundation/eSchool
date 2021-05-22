06796075df78c91a557ec89e8cbacbbdd425f9c4
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-05-22 22:04:34 +0600

    renaming

--- src/Services/CertificateRegistration/CertificateRegistration.API/Startup.cs
+++ src/Services/CertificateRegistration/CertificateRegistration.API/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace CertificateRegistration.API
+namespace CertificateProcessing.API
 {
     public class Startup
     {
@@ -30,7 +23,7 @@ namespace CertificateRegistration.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CertificateRegistration.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CertificateProcessing.API", Version = "v1" });
             });
         }
 
@@ -43,9 +36,9 @@ namespace CertificateRegistration.API
                 app.UseSwagger();
             }
 
-             app.UseSwaggerUI(
-                 c => c.SwaggerEndpoint(
-                     "/swagger/v1/swagger.json", "CertificateRegistration.API v1"));
+            app.UseSwaggerUI(
+                c => c.SwaggerEndpoint(
+                    "/swagger/v1/swagger.json", "CertificateProcessing.API v1"));
 
             app.UseRouting();
 

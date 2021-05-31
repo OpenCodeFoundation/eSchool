fce0ee8762db1d3baebcee42c31e4f05978b5541
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-05-31 21:15:00 +0600

    renamed  CretificateRegistration to CertificatreProcessing

--- src/Services/CertificateProcessing/CertificateProcessing.API/Controllers/HelloWorldController.cs
+++ src/Services/CertificateProcessing/CertificateProcessing.API/Controllers/HelloWorldController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace CertificateRegistration.API.Controllers
+namespace CertificateProcessing.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]

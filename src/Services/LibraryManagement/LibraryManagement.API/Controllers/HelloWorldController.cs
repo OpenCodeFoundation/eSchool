81cc6d2a96c4962dfc94ac2ee4f860d133573e1f
sohan093007 <sohan093007@gmail.com>
2021-06-08 22:33:02 +0600

    PR for LibraryMangment

---
+++ src/Services/LibraryManagement/LibraryManagement.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LibraryManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public static string Ping()
+        {
+            return Ok("Pong");
+        }
+    }
+}

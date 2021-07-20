6005b8de6b63cb2c165a14badf05cc511272fd37
Ratan Sunder Parai <ratan.parai@selise.ch>
2021-07-20 20:51:23 +0600

    value converter

---
+++ src/Services/Enrolling/Enrolling.Infrastructure/Configuration/ValueConverters/EnrollmentIdValueConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using OpenCodeFoundation.ESchool.Services.Enrolling.Domain.AggregatesModel.EnrollmentAggregate;
+
+namespace OpenCodeFoundation.ESchool.Services.Enrolling.Infrastructure.Configuration.ValueConverters
+{
+    public class EnrollmentIdValueConverter
+        : ValueConverter<EnrollmentId, Guid>
+    {
+        public EnrollmentIdValueConverter()
+            : base(e => e.Value, value => EnrollmentId.FromGuid(value))
+        {
+            
+        }
+    }
+}

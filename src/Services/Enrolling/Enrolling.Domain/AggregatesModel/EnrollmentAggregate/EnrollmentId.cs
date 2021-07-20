7afec9906752f04c25afd0010c9525b0c9971946
Ratan Sunder Parai <ratan.parai@selise.ch>
2021-07-20 19:17:22 +0600

    create EntityId

---
+++ src/Services/Enrolling/Enrolling.Domain/AggregatesModel/EnrollmentAggregate/EnrollmentId.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace OpenCodeFoundation.ESchool.Services.Enrolling.Domain.AggregatesModel.EnrollmentAggregate
+{
+    [StronglyTypedId(typeof(Guid))]
+    public partial struct EnrollmentId
+    {
+    }
+}

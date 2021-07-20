f9daf834b64f3f078c3151a666a5ceb4ec26d9dc
Ratan Sunder Parai <ratan.parai@selise.ch>
2021-07-20 20:56:55 +0600

    add query

---
+++ src/Services/Enrolling/Enrolling.API/Application/Queries/GetEnrollmentByIdHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using OpenCodeFoundation.ESchool.Services.Enrolling.Domain.AggregatesModel.EnrollmentAggregate;
+
+namespace OpenCodeFoundation.ESchool.Services.Enrolling.API.Application.Queries
+{
+    public class GetEnrollmentByIdHandler
+        : IRequestHandler<GetEnrollmentByIdQuery, Enrollment>
+    {
+        public Task<Enrollment> Handle(
+            GetEnrollmentByIdQuery query,
+            CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Enrolling/Enrolling.API/Application/Queries/GetEnrollmentByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using OpenCodeFoundation.ESchool.Services.Enrolling.Domain.AggregatesModel.EnrollmentAggregate;
+
+namespace OpenCodeFoundation.ESchool.Services.Enrolling.API.Application.Queries
+{
+    public class GetEnrollmentByIdQuery
+        : IRequest<Enrollment>
+    {
+        public GetEnrollmentByIdQuery(EnrollmentId id)
+        {
+            Id = id;
+        }
+
+        public EnrollmentId Id { get;  }
+    }
+}

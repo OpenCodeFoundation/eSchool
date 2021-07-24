f89ec128ac1917c5c5db920f01b737762c482f42
shahanazdev19 <48450494+shahanazdev19@users.noreply.github.com>
2021-07-24 18:40:23 +0600

    feat(result-processing): initial implementation of result processing service  (#457)
    Co-authored-by: Ratan Sunder Parai <ratanparai@gmail.com>

---
+++ src/Services/ResultProcessing/ResultProcessing.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResultProcessing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Ping()
+        {
+            return Ok("This is new Web API");
+        }
+    }
+}

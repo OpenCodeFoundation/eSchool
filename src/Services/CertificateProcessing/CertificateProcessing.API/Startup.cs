5e03893706625177f2be76e564f06947d1405e3b
Ratan Sunder Parai <ratan.parai@selise.ch>
2021-08-21 20:19:49 +0600

    fix build

--- src/Services/CertificateProcessing/CertificateProcessing.API/Startup.cs
+++ src/Services/CertificateProcessing/CertificateProcessing.API/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Serilog;
 

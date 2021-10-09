3540a56c294c456fbd83dfd5569543402527d194
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-10-09 18:02:53 +0600

    comment removed

--- src/Services/Attendance/Attendance.API/Startup.cs
+++ src/Services/Attendance/Attendance.API/Startup.cs
@@ -16,7 +16,6 @@ namespace Attendance.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -32,7 +31,6 @@ namespace Attendance.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
